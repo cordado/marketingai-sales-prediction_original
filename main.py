8e67b3b7527071f4d0f05658e82e538b2e29b854
cordado <107951840+cordado@users.noreply.github.com>
2024-10-16 15:07:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -171,8 +171,8 @@ if cluster_selecionado != 'Escolha uma opção':
                 top_100_stores = dados_filtrados_loja[['year_month','item','store', 'sales', 'mean_price', 'SOMA']].nlargest(200, 'SOMA')
                 top_100_stores_2015_2016 = top_100_stores[top_100_stores['year_month'].str.startswith(('2015', '2016'))]
                 top_10_rep_2015_2016 = top_100_stores_2015_2016[['item']].value_counts().reset_index()
-                top_5_2015-16 = top_100_stores_2015_2016.value_counts().head(5)
-                st.dataframe(top_5_2015)
+                top_5_2015_16 = top_100_stores_2015_2016.value_counts().head(5)
+                st.dataframe(top_5_2015_16)
                  
                 
                 # Display the DataFrame
